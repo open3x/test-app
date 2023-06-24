a53e2a85a52154822a144fc4efad37e6d5bff7b8
oji3 <oji3xx@gmail.com>
2023-06-24 15:31:01 +0800

    use patterns

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,24 @@
 import { css } from '@/styled-system/css'
+import { container, flex, grid, vstack } from '@/styled-system/patterns'
 import Avatar from "@/components/Avatar"
 
 export default function Home() {
   return (
-    <section>
-      <div className={css({ fontSize: "2xl", fontWeight: 'bold' })}>Hello 🐼!</div>
-      <Avatar />
-    </section>
+    <div className={container()}>
+      <div className={vstack({ gap: '6', padding: '4' })}>
+        <div className={css({ fontSize: "2xl", fontWeight: 'bold' })}>Hello 🐼!</div>
+        <Avatar />
+      </div>
+      <div className={flex({ direction: 'row', align: 'center' })}>
+        <div>First</div>
+        <div>Second</div>
+        <div>Third</div>
+      </div>
+      <div className={grid({ columns: 3, gap: '6' })}>
+        <div>First</div>
+        <div>Second</div>
+        <div>Third</div>
+      </div>
+    </div>
   )
 }
